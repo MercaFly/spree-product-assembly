0bc956b9f45d36d876607a8a72912b942a35983a
simple <simple.continue@gmail.com>
2017-11-30 22:24:15 +0800

    only calculate 1 depth parts

--- app/models/spree/stock/quantifier.rb
+++ app/models/spree/stock/quantifier.rb
@@ -1,17 +1,19 @@
 module Spree
   module Stock
     class Quantifier
-      attr_reader :stock_items, :variant
+      attr_reader :stock_items, :variant, :recursion
   
-      def initialize(variant)
+      def initialize(variant, recursion=true)
         @variant = variant
         @stock_items = @variant.stock_items.with_active_stock_location
+        @recursion = recursion
       end
   
       def total_on_hand
-        if variant.parts.exists?
+        if variant.parts.exists? && recursion
           variant.parts_variants.map do |part|
-            t = Quantifier.new(part.part).total_on_hand
+            ## only calculate 1 depth parts, set recursion false
+            t = Quantifier.new(part.part, false).total_on_hand
             d = part.count
             t/d
           end.min
